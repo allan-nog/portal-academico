ba4bee69c7bba39bf67308cc45c267d8c40d1e4d
Allan Nogueira <allannog.dev@gmail.com>
2025-06-29 15:32:43 -0300

    Atualizando função UserUtils.h

--- src/utils/UserUtils.h
+++ src/utils/UserUtils.h
@@ -1,5 +1,7 @@
 #pragma once
 #include <string>
+#include "../core/Student.h"
+#include "../core/Teacher.h"
 
 /**
  * @brief Função responsável por registrar um usuário no sistema.
@@ -16,3 +18,27 @@
  * - -3: senhas não coincidem
  */
 int registerUser(const std::string &userType);
+
+/**
+ * @brief Salva os dados de um estudante no arquivo correspondente.
+ * 
+ * @param student Objeto Student a ser salvo.
+ */
+void saveStudent(const Student& student);
+
+/**
+ * @brief Salva os dados de um professor no arquivo correspondente.
+ * 
+ * @param teacher Objeto Teacher a ser salvo.
+ */
+void saveTeacher(const Teacher& teacher);
+
+/**
+ * @brief Verifica se um usuário já existe no sistema com base no email e tipo.
+ * 
+ * @param email Email do usuário para verificação.
+ * @param userType Tipo do usuário ("student", "teacher").
+ * @return true Se o usuário já existir.
+ * @return false Se o usuário não existir.
+ */
+bool userExists(const std::string& email, const std::string& userType);
